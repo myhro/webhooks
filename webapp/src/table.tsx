f3494b3cc8aff67002125d92bf9c9784574b6622
Tiago Ilieve <tiago.myhro@gmail.com>
2020-01-16 08:30:50 -0300

    table: use components instead of functions

--- webapp/src/table.tsx
+++ webapp/src/table.tsx
@@ -14,57 +14,63 @@ type TableProps = {
   webhooks: Webhook[];
 };
 
-function Headers(props: HeadersProps): JSX.Element {
-  return (
-    <div className="headers">
-      {Object.keys(props.headers).map((key: string, i: number) => (
-        <span key={i}>
-          <strong>{key}</strong>: {props.headers[key]}
-          <br />
-        </span>
-      ))}
-    </div>
-  );
-}
-
-function Row(props: RowProps): JSX.Element {
-  return (
-    <tr>
-      <td>{props.webhook.date}</td>
-      <td>{props.webhook.ip}</td>
-      <td>{props.webhook.proto}</td>
-      <td>{props.webhook.method}</td>
-      <td>
-        <Headers headers={props.webhook.headers} />
-      </td>
-      <td>{props.webhook.body}</td>
-    </tr>
-  );
-}
-
-function Table(props: TableProps): JSX.Element {
-  if (props.webhooks.length > 0) {
+class HeadersColumn extends React.Component<HeadersProps, {}> {
+  render(): JSX.Element {
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>IP</th>
-            <th>Protocol</th>
-            <th>Method</th>
-            <th>Headers</th>
-            <th>Body</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.webhooks.map((w: Webhook, i: number) => (
-            <Row key={i} webhook={w} />
-          ))}
-        </tbody>
-      </table>
+      <div className="headers">
+        {Object.keys(this.props.headers).map((key: string, i: number) => (
+          <span key={i}>
+            <strong>{key}</strong>: {this.props.headers[key]}
+            <br />
+          </span>
+        ))}
+      </div>
     );
   }
-  return <React.Fragment />;
+}
+
+class Row extends React.Component<RowProps, {}> {
+  render(): JSX.Element {
+    return (
+      <tr>
+        <td>{this.props.webhook.date}</td>
+        <td>{this.props.webhook.ip}</td>
+        <td>{this.props.webhook.proto}</td>
+        <td>{this.props.webhook.method}</td>
+        <td>
+          <HeadersColumn headers={this.props.webhook.headers} />
+        </td>
+        <td>{this.props.webhook.body}</td>
+      </tr>
+    );
+  }
+}
+
+class Table extends React.Component<TableProps, {}> {
+  render(): JSX.Element {
+    if (this.props.webhooks.length > 0) {
+      return (
+        <table>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>IP</th>
+              <th>Protocol</th>
+              <th>Method</th>
+              <th>Headers</th>
+              <th>Body</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.webhooks.map((w: Webhook, i: number) => (
+              <Row key={i} webhook={w} />
+            ))}
+          </tbody>
+        </table>
+      );
+    }
+    return <React.Fragment />;
+  }
 }
 
 export default Table;
